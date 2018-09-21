fc25146d29fc0b11747a401a0c627d09882026df
Kiran Joy <kijoyin@outlook.com>
2018-09-21 12:46:58 +1000

    full car code

--- CarServer/control_car.c
+++ CarServer/control_car.c
@@ -1,13 +1,14 @@
 #include <wiringPi.h>
 #include <stdbool.h>
 #include <softPwm.h>
+#include "include\control_car.h"
 
 static int leftForwardPin = 28;
 static int leftBackwardPin = 29;
 static int rightForwardPin = 24;
 static int rightBackwardPin = 25;
 static int pmwPin = 1;
-static int speed = 5;
+static int speed = 800;
 static bool forward = true;
 
 void init_car()
@@ -19,28 +20,107 @@ void init_car()
 	pinMode(leftBackwardPin, OUTPUT);
 	pinMode(rightForwardPin, OUTPUT);
 	pinMode(rightBackwardPin, OUTPUT);
-	digitalWrite(leftBackwardPin, LOW);
-	digitalWrite(leftForwardPin, LOW);
-	digitalWrite(rightBackwardPin, LOW);
-	digitalWrite(rightForwardPin, LOW);
-	pwmWrite(pmwPin, 800);
+	AllLow();
+	pwmWrite(pmwPin, speed);
 }
 void start_car()
 {
 	speed = 50;
 	forward = true;
-	pwmWrite(pmwPin, 800);
+	pwmWrite(pmwPin, speed);
 	digitalWrite(leftBackwardPin, LOW);
 	digitalWrite(leftForwardPin, HIGH);
 	digitalWrite(rightBackwardPin, LOW);
 	digitalWrite(rightForwardPin, HIGH);
-	//pwmWrite(pmwPin, speed);
 }
 void stop_car()
 {
-	speed = 50;
 	forward = true;
+	AllLow();
+
+}
+void reverse_car()
+{
+	forward = false;
+	digitalWrite(leftBackwardPin, HIGH);
 	digitalWrite(leftForwardPin, LOW);
+	digitalWrite(rightBackwardPin, HIGH);
 	digitalWrite(rightForwardPin, LOW);
-	//pwmWrite(pmwPin, 0);
+}
+
+void turn_left()
+{
+	if (forward)
+	{
+		stop_start_left();
+	}
+	else
+	{
+		stop_start_right();
+	}
+}
+
+void turn_right()
+{
+	if (forward)
+	{
+		stop_start_right();
+	}
+	else
+	{
+		stop_start_left();
+	}
+}
+
+void stop_start_left()
+{
+	digitalWrite(leftBackwardPin, LOW);
+	digitalWrite(leftForwardPin, LOW);
+	delayMicroseconds(10);
+	if (forward)
+	{
+		digitalWrite(leftForwardPin, HIGH);
+	}
+	else
+	{
+		digitalWrite(leftBackwardPin, HIGH);
+	}
+}
+
+
+void stop_start_right()
+{
+	digitalWrite(rightBackwardPin, LOW);
+	digitalWrite(rightForwardPin, LOW);
+	delayMicroseconds(10);
+	if (forward)
+	{
+		digitalWrite(rightForwardPin, HIGH);
+	}
+	else
+	{
+		digitalWrite(rightBackwardPin, HIGH);
+	}
+}
+
+void accelerate(bool acc)
+{
+	if (acc && speed < 1000)
+	{
+		speed = speed + 50;
+		pwmWrite(pmwPin, speed);
+	}
+	else if (!acc && speed < 200)
+	{
+		speed = speed - 50;
+		pwmWrite(pmwPin, speed);
+	}
+}
+
+void AllLow()
+{
+	digitalWrite(leftBackwardPin, LOW);
+	digitalWrite(leftForwardPin, LOW);
+	digitalWrite(rightBackwardPin, LOW);
+	digitalWrite(rightForwardPin, LOW);
 }
--- CarServer/include/control_car.h
+++ CarServer/include/control_car.h
@@ -3,4 +3,11 @@
 void init_car();
 void start_car();
 void stop_car();
+void reverse_car();
+void turn_left();
+void turn_right();
+void stop_start_left();
+void stop_start_right();
+void accelerate(bool acc);
+void AllLow();
 
